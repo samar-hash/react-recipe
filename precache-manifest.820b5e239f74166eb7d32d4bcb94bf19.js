bc111b2a6be8324fd099657cb229ba438c470a13
samarmahmoud <samarmahmoud1516@gmail.com>
2020-04-22 06:16:32 +0200

    Updates

---
+++ precache-manifest.820b5e239f74166eb7d32d4bcb94bf19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "381562985de47972ec74",
+    "url": "/react-recipe/static/css/main.5899c364.chunk.css"
+  },
+  {
+    "revision": "381562985de47972ec74",
+    "url": "/react-recipe/static/js/main.6e489512.chunk.js"
+  },
+  {
+    "revision": "1f31941b1e9db2686091",
+    "url": "/react-recipe/static/js/runtime~main.dbd27b7b.js"
+  },
+  {
+    "revision": "99219a8015fb22fa48a0",
+    "url": "/react-recipe/static/css/2.8029c572.chunk.css"
+  },
+  {
+    "revision": "99219a8015fb22fa48a0",
+    "url": "/react-recipe/static/js/2.4263f613.chunk.js"
+  },
+  {
+    "revision": "02044c8060c2e6f2490b8bd43042c5f6",
+    "url": "/react-recipe/index.html"
+  }
+];
